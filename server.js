9c2411cb978d9917701d1eca833365511c8250e6
Roman Kazarin <rkazarin@Romans-MacBook-Air.local>
2014-07-29 18:39:53 -0700

    login/logout + getApiKeys + endpoint

--- server.js
+++ server.js
@@ -1,26 +1,96 @@
 //server.js
 
+//Server Setup
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var stormpath = require('express-stormpath');
+var http = require("http");
 
 server.use(bodyParser());
+server.use(stormpath.init(server, {
+	apiKeyFile: '/Users/rkazarin/.stormpath/apiKey.properties',
+	application: 'https://api.stormpath.com/v1/applications/5HRNSljrcYQax0oCQQovT9',
+	secretKey: 'askjdhaslkjdhwlieuhdwljihdiu2yrp9823yrilhp9823yerhiuqhrp9283yehiuqwhep81732eyhiquwoghp8971yehilqusjd',
+	redirectUrl: '/'
+}))
+
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
 
 var port = process.env.PORT || 8080;
 
+//Routing
 server.get('/', function(req, res) {
 	res.render("login.ejs");
 });
 
+server.get('/dashboard', stormpath.loginRequired, function(req, res) {
+
+	var apiKeyId = "";
+	var apiKeySecret = "";
+
+	res.locals.user.getApiKeys(function(err, collectionResult) {
+		if(collectionResult.items == '[]') {
+			console.log("Creating new api key");
+			res.locals.user.createApiKey(function(err, apiKey) {
+				res.locals.apiKeyId = apiKey.id;
+				res.locals.apiKeySecret = apiKey.secret; 
+				res.render("dashboard.ejs");
+				return;	
+			})
+
+		}
+		else {
+			console.log("API Key exists");
+			collectionResult.each(function(apiKey) {
+				res.locals.apiKeyId = apiKey.id;
+				res.locals.apiKeySecret = apiKey.secret;
+				res.locals.username = res.locals.user.username;
+				res.render("dashboard.ejs");
+				return;
+			})
+		}				
+	})
+});
+
+server.get('/weather/:city', function(req, res) {
+
+	console.log("Getting weather for " + req.params.city);
+
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=" + req.params.city;
+
+	var data = "";
+
+	http.get(url, function(myRes) {
+		myRes.on('data', function(chunk) {
+			data += chunk;
+		});
+		myRes.on('end', function() {
+			callback(data);
+		});
+	}).on('error', function() {
+		console.log("fuuuuhhh");
+	});
+
+	function callback(finalData) {
+		var json = JSON.parse(finalData);
+		console.log(json);
+		console.log(json.main.temp);
+
+		//convert to Farenheight
+		var farenheight = Math.round((((parseFloat(json.main.temp) - 273.15) * 1.8) + 32) * 10)/10;
+		res.json(farenheight);
+
+	}
+});
+
+// server.post('/oauthToken')
+
+//More Setup
 server.use(express.static(__dirname + '/static'));
 
 server.listen(port);
 console.log('Magic happens on port ' + port);
-
-
-
--- static/app.js
+++ static/app.js
@@ -1,11 +1,25 @@
 var myApp = angular.module('loginForm', []);
 
-myApp.service('sharedProperties', function() {
-    var apiKey = "";
-    var apiSecret = "";
-    var encodedAuth = "";
+myApp.service('sharedProperties', function($window) {
+    var toE
     var oauthToken = "";
     var userName = "";
+    var apiKey = $window.apiKeyId;
+    var apiSecret = $window.apiKeySecret;
+
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length)
+        {n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.
+            charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.
+            indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.
+            fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++)
+        {var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);
+            t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);
+            t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+    if(apiKey != undefined){
+        var toEncode = apiKey.concat(':', apiSecret);
+        var encodedAuth = Base64.encode(toEncode);
+    }       
 
     return {
         getApiKey: function() {
@@ -43,21 +57,11 @@ myApp.service('sharedProperties', function() {
 
 myApp.controller('loginController', ["$scope", "$window", "$http", 'sharedProperties', function($scope, $window, $http, sharedProperties) {
 
+    console.log("Initializing loginController");
+
     $scope.submitFunction = function() {
 
-        var username = $scope.userName;
-        var password = $scope.password;
-
-        //Try to log in to account
-        $http({method: "POST", url: '/rest/login',
-            data: {'Username': username, 'Password': password}
-        }).success(function(data, status, headers, config) {
-                $window.location.href = "/dashboard.html";
-
-            }).
-            error(function(data, status, headers, config) {
-                $window.alert("Wrong username/password. Please try again");
-            });
+        $window.location.href = "/login?next=%2Fdashboard";
     }
 }]);
 
@@ -65,14 +69,8 @@ myApp.controller('logoutController', ["$scope", "$window", "$http", function($sc
 
     $scope.makeLogoutCall = function() {
 
-        $http({method: "GET", url: '/rest/logout',
-        }).success(function(data, status, headers, config) {
-                $window.location.href = "/login.html";
+        $window.location.href = "/logout";
 
-            }).
-            error(function(data, status, headers, config) {
-                $window.alert("Log out error");
-            });
     }
 }]);
 
@@ -80,71 +78,11 @@ myApp.controller('makeAccountController', ["$scope", "$http", "$window", functio
 
     $scope.signUp = function() {
 
-        //Get data from Create New account form
-        var firstName = $scope.firstName;
-        var lastName = $scope.lastName;
-        var userName = $scope.userName;
-        var email = $scope.email;
-        var password = $scope.password;
-
-        //Try to create the account
-        $http({method: "POST", url: '/rest/makeStormpathAccount',
-               data: {'first_name': firstName, 'last_name': lastName, 'user_name': userName, 'email': email, 'password': password}
-        }).success(function(data, status, headers, config) {
-
-            //Clear Create Account fields
-            $scope.firstName = "";
-            $scope.lastName = "";
-            $scope.userName = "";
-            $scope.email = "";
-            $scope.password = "";
-            $window.alert("Account Created! Now, Sign In!");
-
-        }).
-            error(function(data, status, headers, config) {
-                $window.alert("Error");
-        });
+        $window.location.href = "/register";
+    
     }
 
 }]);
-
-myApp.controller('ApiKeyController', ["$scope", "$http", 'sharedProperties', '$window', function($scope, $http, sharedProperties, $window) {
-
-    //Get an API key and secret
-    $http({method: "GET", url: '/rest/getApiKey'}).success(function(data, status, headers, config) {
-
-        $scope.apiKey = data.api_key;
-        $scope.apiSecret = data.api_secret;
-        var username = data.username;
-        $scope.userName = username;
-
-        sharedProperties.setApiKey($scope.apiKey);
-        sharedProperties.setApiSecret($scope.apiSecret);
-
-        //Base64 encode the key and secret and save to shared properties
-        var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length)
-        {n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.
-            charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.
-            indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.
-            fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++)
-        {var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);
-            t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);
-            t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-
-        var toEncode = sharedProperties.getApiKey().concat(':', sharedProperties.getApiSecret());
-        var encodedAuth = Base64.encode(toEncode);
-        sharedProperties.setEncodedAuth(encodedAuth);
-        sharedProperties.setUsername(username);
-
-
-    })
-        .error(function(data, status, headers, config) {
-            $window.alert("Please log in");
-            $window.location.href = "/login.html";
-        });
-
-}]);
-
 
 myApp.controller('RestBasicController', ["$scope", "$window", "$http", 'sharedProperties', function($scope, $window, $http, sharedProperties) {
 
@@ -153,8 +91,8 @@ myApp.controller('RestBasicController', ["$scope", "$window", "$http", 'sharedPr
     $scope.makeRestCall = function() {
 
         //Get the temperature of specified city from REST endpoint, using Basic Auth
-        $http({method: "GET", url: '/rest/api/weather/' + $scope.city, headers: {'Authorization': 'Basic ' + sharedProperties.getEncodedAuth()}}).success(function(data, status, headers, config) {
-            $scope.temp = data;
+        $http({method: "GET", url: '/weather/' + $scope.city, headers: {'Authorization': 'Basic ' + sharedProperties.getEncodedAuth()}}).success(function(data, status, headers, config) {
+            $scope.temp = data + ' F';
             $scope.myCity = $scope.city;
 
         })
@@ -196,7 +134,7 @@ myApp.controller('OauthTokenController', ["$scope", "$http", "$window",'sharedPr
         console.log(myData);
 
         //Try and get an Oauth Token
-        $http({method: "POST", url: '/rest/oauthToken',
+        $http({method: "POST", url: '/oauthToken',
             headers: {'Authorization': 'Basic ' + sharedProperties.getEncodedAuth(), 'Content-Type': 'application/x-www-form-urlencoded'},
             data : myData})
 
@@ -212,8 +150,6 @@ myApp.controller('OauthTokenController', ["$scope", "$http", "$window",'sharedPr
                 $window.alert("Error");
             });
         }
-
-
 }]);
 
 
